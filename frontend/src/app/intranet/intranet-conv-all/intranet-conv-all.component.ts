fee7d0677ca0926307a16ca96303e6b14948a299
Jeff269 <mini4tur4@gmail.com>
2022-04-24 09:19:10 -0500

    22/04/24

--- frontend/src/app/intranet/intranet-conv-all/intranet-conv-all.component.ts
+++ frontend/src/app/intranet/intranet-conv-all/intranet-conv-all.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-intranet-conv-all',
   templateUrl: './intranet-conv-all.component.html',
-  styleUrls: ['./intranet-conv-all.component.css']
+  styleUrls: ['./intranet-conv-all.component.css','../intranet.component.css']
 })
 export class IntranetConvAllComponent implements OnInit {
 
--- frontend/src/app/intranet/intranet-register/intranet-register.component.ts
+++ frontend/src/app/intranet/intranet-register/intranet-register.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-intranet-register',
   templateUrl: './intranet-register.component.html',
-  styleUrls: ['./intranet-register.component.css']
+  styleUrls: ['./intranet-register.component.css','../intranet-login/intranet-login.component.css']
 })
 export class IntranetRegisterComponent implements OnInit {
 
